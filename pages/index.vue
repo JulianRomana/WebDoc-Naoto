daa2b3a3cb49efca959aab30c136485697b6f2e1
JulianRomana <jjulian.romana@gmail.com>
2019-06-19 16:42:14 +0200

    basic nuxt project

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="screen">
-    <h1>La base</h1>
+    <h1>Accueil</h1>
+    <nuxt-link class="nuxt-links" to="/posts">Voir les posts</nuxt-link>
   </div>
 </template>
 
@@ -15,8 +16,21 @@ export default {
 </script>
 
 <style scoped lang="scss">
+.screen {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  height: 50vh;
+}
 h1 {
   text-align: center;
   font-family: Verdana, Geneva, Tahoma, sans-serif;
+  font-size: 36px;
+}
+.links {
+  font-size: 20px;
+  color: black;
+  text-decoration: underline;
 }
 </style>
