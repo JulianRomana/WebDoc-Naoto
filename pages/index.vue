152417c878fb609821173f3e3a06cf09f1d621c3
simtaxx <souici.haris@gmail.com>
2019-06-20 17:10:32 +0200

    index update

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,16 @@
 <template>
   <div class="screen">
-    <h1>Accueil</h1>
-    <nuxt-link class="nuxt-links" to="/posts">Voir les posts</nuxt-link>
+    <div class="Title">
+      <h1>FUKUSHIMA</h1>
+      <p>NAOTO MATSUMURA : LE GARDIEN DES ANIMAUX</p>
+    </div>
+    <div class="Skip">
+      <nuxt-link class="nuxt-links" to="/page2">
+        <img src="../static/images/arrow.svg">
+        <p>PASSER L'INTRO</p>
+      </nuxt-link>
+    </div>
+    <video src="../assets/videos/intro_webdoc.mp4" autoplay></video>
   </div>
 </template>
 
@@ -15,16 +24,78 @@ export default {};
   flex-direction: column;
   align-items: center;
   justify-content: space-between;
-  height: 50vh;
+  height: 100vh;
 }
-h1 {
-  text-align: center;
-  font-family: Verdana, Geneva, Tahoma, sans-serif;
-  font-size: 36px;
+.screen::before {
+  content: "";
+  display: block;
+  width: 100vw;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.541);
+  position: absolute;
+  z-index: -2;
+  animation: filterOut 2s forwards;
+  animation-delay: 4s;
+}
+.Title {
+  z-index: 1;
+  position: absolute;
+  right: 10vw;
+  top: 300px;
+  font-family: League spartan;
+  color: white;
+  animation: filterOut 2s forwards;
+  animation-delay: 4s;
+  h1 {
+    font-size: 90px;
+  }
+  p {
+    font-size: 22px;
+  }
+}
+.Skip {
+  z-index: 1;
+  color: white;
+  font-family: League spartan;
+  position: absolute;
+  bottom: 5%;
+  right: 5%;
+
+  .nuxt-links {
+    color: white;
+    text-decoration: none;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    p {
+      font-size: 16px;
+    }
+  }
+  img {
+    padding-right: 25px;
+  }
+}
+video {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  min-width: 100%;
+  min-height: 100%;
+  transform: translateX(-50%) translateY(-50%);
+  z-index: -3;
 }
 .links {
   font-size: 20px;
   color: black;
   text-decoration: underline;
+}
+@keyframes filterOut {
+  from {
+    opacity: 1;
+  }
+
+  to {
+    opacity: 0;
+  }
 }
 </style>
