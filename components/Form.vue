b4fadd1b70eea8524d30b3c0e86f9c33ddc53fc8
JulianRomana <jjulian.romana@gmail.com>
2019-06-20 10:25:05 +0200

    form component

---
+++ components/Form.vue
@@ -0,0 +1,53 @@
+<template>
+  <form id="form" action @submit="checkForm()" method="post">
+    <h1>S'identifier</h1>
+    <label for="id">Identifiant</label>
+    <input v-model="id" class="inputs" type="text" id="id" name="id">
+    <label for="password">Mot de passe</label>
+    <input v-model="password" class="inputs" type="password" id="password" name="password">
+    <input class="button" type="submit" value="Se connecter">
+  </form>
+</template>
+<style>
+form {
+  position: absolute;
+  bottom: 45vh;
+  left: 40vw;
+  display: flex;
+  flex-direction: column;
+}
+label {
+  font-size: 20px;
+  color: white;
+}
+.inputs {
+  border-radius: 7px;
+  width: 350px;
+  height: 30px;
+  border: none;
+  opacity: 0.6;
+  font-size: 16px;
+}
+.button {
+  border-radius: 5px;
+  font-size: 20px;
+  margin-left: 25%;
+  margin-top: 20px;
+  height: 40px;
+  background-color: #aecc58;
+  width: 155px;
+  border: none;
+  color: white;
+}
+</style>
+<script>
+export default {
+  props: ["FormConnexion"],
+  data() {
+    return {
+      id: null,
+      password: null
+    };
+  }
+};
+</script>
--- pages/login.vue
+++ pages/login.vue
@@ -1,13 +1,6 @@
 <template>
   <div class="screen">
-    <form action @submit="checkForm()" method="post">
-      <h1>S'identifier</h1>
-      <label for="id">Identifiant</label>
-      <input class="inputs" v-model="id" type="text" id="id" name="id">
-      <label for="password">Mot de passe</label>
-      <input class="inputs" v-model="password" type="password" id="password" name="password">
-      <input class="button" type="submit" value="Se connecter">
-    </form>
+    <FormConnexion></FormConnexion>
   </div>
 </template>
 
@@ -34,42 +27,13 @@ h1 {
   height: 100vh;
   background-color: rgba(0, 0, 0, 0.541);
 }
-form {
-  position: absolute;
-  bottom: 45vh;
-  left: 40vw;
-  display: flex;
-  flex-direction: column;
-}
-label {
-  font-size: 20px;
-  color: white;
-}
-.inputs {
-  border-radius: 7px;
-  width: 350px;
-  height: 30px;
-  border: none;
-  opacity: 0.6;
-  font-size: 16px;
-}
-.button {
-  border-radius: 5px;
-  font-size: 20px;
-  margin-left: 25%;
-  margin-top: 20px;
-  height: 40px;
-  background-color: #aecc58;
-  width: 155px;
-  border: none;
-  color: white;
-}
 </style>
 <script>
+import FormConnexion from "../components/Form";
+
 export default {
-  data: {
-    id: null,
-    password: null
+  components: {
+    FormConnexion
   }
 };
 </script>
