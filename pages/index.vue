1f36b241d684d0e14260c939146ce164a762fb71
simtaxx <souici.haris@gmail.com>
2019-06-21 14:56:43 +0200

    second screen up

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,43 @@
 <template>
-  <div class="screen">
-    <div class="Title">
-      <h1>FUKUSHIMA</h1>
-      <p>NAOTO MATSUMURA : LE GARDIEN DES ANIMAUX</p>
-    </div>
-    <div class="Skip">
-      <nuxt-link class="nuxt-links" to="/page2">
+  <div>
+    <section id="firstScreen" class="firstScreen">
+      <div class="byeTitle">
+        <h1>FUKUSHIMA</h1>
+        <p>NAOTO MATSUMURA : LE GARDIEN DES ANIMAUX</p>
+      </div>
+      <div class="Skip" v-on:click="nextScreen">
         <img src="../static/images/arrow.svg">
         <p>PASSER L'INTRO</p>
-      </nuxt-link>
-    </div>
-    <video src="../assets/videos/intro_webdoc.mp4" autoplay></video>
+      </div>
+      <video src="../assets/videos/intro_webdoc.mp4" autoplay></video>
+    </section>
+    <section id="secondScreen" class="secondScreen invisible">
+      <span>
+        {{ msg }}
+      </span>
+      <div class="Title">
+        <h1>NAOTO MATSUMURA</h1>
+        <p>20 min</p>
+        <p
+          class="description"
+        >Un webdocumentaire narrant le parcours </br> d’un homme revenu vivre dans son village </br> irradié.</p>
+      </div>
+      <div class="previous" v-on:click="previousScreen">
+        <img src="../static/images/arrow2.svg">
+        <p>AVANT</p>
+      </div>
+      <div class="play">
+        <div>
+          <img src="../static/images/line.svg">
+          <img src="../static/images/arrow2.svg">
+        </div>
+        <p>Commencer l'expèrience</p>
+      </div>
+      <div class="Skip">
+        <img src="../static/images/arrow2.svg">
+        <p>FUKUSHIMA</p>
+      </div>
+    </section>
   </div>
 </template>
 
@@ -19,14 +46,14 @@ export default {};
 </script>
 
 <style scoped lang="scss">
-.screen {
+.firstScreen {
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: space-between;
   height: 100vh;
 }
-.screen::before {
+.firstScreen::before {
   content: "";
   display: block;
   width: 100vw;
@@ -37,7 +64,46 @@ export default {};
   animation: filterOut 2s forwards;
   animation-delay: 4s;
 }
-.Title {
+.secondScreen {
+  background-image: url("../static/images/pic01.jpg");
+  background-size: cover;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
+
+  span {
+    font-family: League spartan;
+    position: absolute;
+    top: 20px;
+    left: 20px;
+  }
+  .Title {
+    z-index: 1;
+    font-family: League spartan;
+    h1 {
+      font-size: 50px;
+    }
+    p {
+      font-family: Roboto;
+      font-size: 20px;
+    }
+  }
+}
+.secondScreen::before {
+  content: "";
+  display: block;
+  width: 100vw;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.541);
+  position: absolute;
+  z-index: 0;
+}
+.invisible {
+  display: none;
+}
+.byeTitle {
   z-index: 1;
   position: absolute;
   right: 10vw;
@@ -60,19 +126,50 @@ export default {};
   position: absolute;
   bottom: 5%;
   right: 5%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 
-  .nuxt-links {
-    color: white;
-    text-decoration: none;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    p {
-      font-size: 16px;
-    }
-  }
   img {
     padding-right: 25px;
+  }
+}
+.previous {
+  z-index: 1;
+  color: white;
+  font-family: League spartan;
+  position: absolute;
+  bottom: 5%;
+  left: 5%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  img {
+    padding-right: 25px;
+  }
+}
+.play {
+  z-index: 1;
+  position: absolute;
+  bottom: 5%;
+  display: flex;
+  flex-direction: row;
+  align-items: flex-end;
+  font-family: RobotoMedium;
+  padding-left: 6%; 
+
+  div {
+  display: flex;
+  flex-direction: column;
+  }
+  p {
+    padding-bottom: 18px;
+    padding-left: 20px;
+  }
+  img:nth-child(1) {
+    padding-bottom: 20px;
+    height: 180px;
   }
 }
 video {
@@ -99,3 +196,28 @@ video {
   }
 }
 </style>
+<script>
+export default {
+  data: function() {
+    return {
+    invisible: false,
+    msg: "L'ARCHE DE TOMIOKA"
+    }
+  },
+  methods: {
+    nextScreen() {
+      let screen = document.getElementById("firstScreen");
+      let screen2 = document.getElementById("secondScreen");
+      screen.classList.add("invisible");
+      screen2.classList.remove("invisible");
+    },
+    previousScreen() {
+      let screen = document.getElementById("secondScreen");
+      let screen2 = document.getElementById("firstScreen");
+      screen.classList.add("invisible");
+      screen2.classList.remove("invisible");
+      console.log("lourd")
+    }
+  }
+};
+</script>
