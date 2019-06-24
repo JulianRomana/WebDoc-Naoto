3b0d0fc7cb7eeb32b55debf59d86e8c7f5c6c1f7
simtaxx <souici.haris@gmail.com>
2019-06-24 11:34:58 +0200

    menu vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,42 +1,15 @@
 <template>
   <div>
     <section id="firstScreen" class="firstScreen">
-      <div class="byeTitle">
-        <h1>FUKUSHIMA</h1>
-        <p>NAOTO MATSUMURA : LE GARDIEN DES ANIMAUX</p>
+      <div class="Title">
+        <h1>L’ARCHE DE TOMIOKA</h1>
+        <p>NAOTO MASTUMURA : LE GARDIEN DES ANIMAUX</p>
       </div>
-      <div class="Skip" v-on:click="nextScreen">
+      <nuxt-link class="Skip" to="/menu">
         <img src="../static/images/arrow.svg">
         <p>PASSER L'INTRO</p>
-      </div>
+      </nuxt-link>
       <video src="../assets/videos/intro_webdoc.mp4" autoplay></video>
-    </section>
-    <section id="secondScreen" class="secondScreen invisible">
-      <span>
-        {{ msg }}
-      </span>
-      <div class="Title">
-        <h1>NAOTO MATSUMURA</h1>
-        <p>20 min</p>
-        <p
-          class="description"
-        >Un webdocumentaire narrant le parcours </br> d’un homme revenu vivre dans son village </br> irradié.</p>
-      </div>
-      <div class="previous" v-on:click="previousScreen">
-        <img src="../static/images/arrow2.svg">
-        <p>AVANT</p>
-      </div>
-      <div class="play">
-        <div>
-          <img src="../static/images/line.svg">
-          <img src="../static/images/arrow2.svg">
-        </div>
-        <p>Commencer l'expèrience</p>
-      </div>
-      <div class="Skip">
-        <img src="../static/images/arrow2.svg">
-        <p>FUKUSHIMA</p>
-      </div>
     </section>
   </div>
 </template>
@@ -52,6 +25,47 @@ export default {};
   align-items: center;
   justify-content: space-between;
   height: 100vh;
+  .Title {
+    z-index: 1;
+    position: absolute;
+    left: 1vw;
+    top: 150px;
+    font-family: League spartan;
+    color: white;
+    animation: filterOut 2s forwards;
+    animation-delay: 4s;
+    h1 {
+      font-size: 80px;
+    }
+    p {
+      font-size: 28px;
+    }
+  }
+  .Skip {
+    z-index: 1;
+    color: white;
+    font-family: League spartan;
+    position: absolute;
+    bottom: 5%;
+    right: 5%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-decoration: none;
+
+    img {
+      padding-right: 25px;
+    }
+  }
+  video {
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    min-width: 100%;
+    min-height: 100%;
+    transform: translateX(-50%) translateY(-50%);
+    z-index: -3;
+  }
 }
 .firstScreen::before {
   content: "";
@@ -64,128 +78,6 @@ export default {};
   animation: filterOut 2s forwards;
   animation-delay: 4s;
 }
-.secondScreen {
-  background-image: url("../static/images/pic01.jpg");
-  background-size: cover;
-  height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  color: white;
-
-  span {
-    font-family: League spartan;
-    position: absolute;
-    top: 20px;
-    left: 20px;
-  }
-  .Title {
-    z-index: 1;
-    font-family: League spartan;
-    h1 {
-      font-size: 50px;
-    }
-    p {
-      font-family: Roboto;
-      font-size: 20px;
-    }
-  }
-}
-.secondScreen::before {
-  content: "";
-  display: block;
-  width: 100vw;
-  height: 100vh;
-  background-color: rgba(0, 0, 0, 0.541);
-  position: absolute;
-  z-index: 0;
-}
-.invisible {
-  display: none;
-}
-.byeTitle {
-  z-index: 1;
-  position: absolute;
-  right: 10vw;
-  top: 300px;
-  font-family: League spartan;
-  color: white;
-  animation: filterOut 2s forwards;
-  animation-delay: 4s;
-  h1 {
-    font-size: 90px;
-  }
-  p {
-    font-size: 22px;
-  }
-}
-.Skip {
-  z-index: 1;
-  color: white;
-  font-family: League spartan;
-  position: absolute;
-  bottom: 5%;
-  right: 5%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-  img {
-    padding-right: 25px;
-  }
-}
-.previous {
-  z-index: 1;
-  color: white;
-  font-family: League spartan;
-  position: absolute;
-  bottom: 5%;
-  left: 5%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-  img {
-    padding-right: 25px;
-  }
-}
-.play {
-  z-index: 1;
-  position: absolute;
-  bottom: 5%;
-  display: flex;
-  flex-direction: row;
-  align-items: flex-end;
-  font-family: RobotoMedium;
-  padding-left: 6%; 
-
-  div {
-  display: flex;
-  flex-direction: column;
-  }
-  p {
-    padding-bottom: 18px;
-    padding-left: 20px;
-  }
-  img:nth-child(1) {
-    padding-bottom: 20px;
-    height: 180px;
-  }
-}
-video {
-  position: fixed;
-  top: 50%;
-  left: 50%;
-  min-width: 100%;
-  min-height: 100%;
-  transform: translateX(-50%) translateY(-50%);
-  z-index: -3;
-}
-.links {
-  font-size: 20px;
-  color: black;
-  text-decoration: underline;
-}
 @keyframes filterOut {
   from {
     opacity: 1;
@@ -196,28 +88,3 @@ video {
   }
 }
 </style>
-<script>
-export default {
-  data: function() {
-    return {
-    invisible: false,
-    msg: "L'ARCHE DE TOMIOKA"
-    }
-  },
-  methods: {
-    nextScreen() {
-      let screen = document.getElementById("firstScreen");
-      let screen2 = document.getElementById("secondScreen");
-      screen.classList.add("invisible");
-      screen2.classList.remove("invisible");
-    },
-    previousScreen() {
-      let screen = document.getElementById("secondScreen");
-      let screen2 = document.getElementById("firstScreen");
-      screen.classList.add("invisible");
-      screen2.classList.remove("invisible");
-      console.log("lourd")
-    }
-  }
-};
-</script>
---
+++ pages/menu.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <section id="menuScreen" class="menuScreen">
+      <img class="logo" src="../static/images/logo.png">
+      <div class="menuTitle">
+        <h1>L’ARCHE DE TOMIOKA</h1>
+        <p>NAOTO MASTUMURA : LE GARDIEN DES ANIMAUX</p>
+      </div>
+      <ul>
+        <li>FUKUSHIMA</li>
+        <li>NAOTO MATSUMURA</li>
+        <li>APRES</li>
+      </ul>
+    </section>
+  </div>
+</template>
+<style scoped lang="scss">
+.menuScreen {
+  background-image: url("../static/images/menuFond.jpg");
+  background-size: cover;
+  background-position: center;
+  height: 100vh;
+  width: 100vw;
+
+  .logo {
+    width: 200px;
+    position: absolute;
+    right: 10px;
+  }
+  .menuTitle {
+    position: absolute;
+    left: 1vw;
+    top: 150px;
+    font-family: League spartan;
+    color: white;
+    h1 {
+      font-size: 80px;
+    }
+    p {
+      font-size: 28px;
+    }
+  }
+  ul {
+    text-align: center;
+    display: flex;
+    justify-content: space-between;
+    align-items: flex-end;
+    position: absolute;
+    left: 5%;
+    bottom: 20px;
+    width: 90%;
+    color: white;
+    font-family: League spartan;
+    li {
+      border-bottom: 1px solid;
+    }
+  }
+}
+</style>
+<script>
+export default {};
+</script>
