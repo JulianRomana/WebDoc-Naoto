2ac43471d8d4cc4bc49450843bc0bef45fdc40c8
JulianRomana <jjulian.romana@gmail.com>
2019-06-25 11:37:45 +0200

    page enfant miracle

---
+++ components/Menu.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="menu__wrapper">
+    <nav class="menu">
+      <ul class="menu__list">
+        <li class="menu__item firstElement">MENU</li>
+        <li class="menu__item">L'ARCHE DE TOMIOKA</li>
+        <li class="menu__item">FUKUSHIMA</li>
+        <li class="menu__item">NAOTO MATSUMURA</li>
+        <li class="menu__item">L'APRES</li>
+      </ul>
+    </nav>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.menu__wrapper {
+  position: absolute;
+  bottom: 0;
+  width: 100vw;
+}
+
+.menu__item:not(.firstElement) {
+  opacity: 0;
+}
+.menu__item {
+  color: white;
+  transition: 0.1s linear;
+}
+.menu__list {
+  position: relative;
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  height: 100%;
+}
+.menu {
+  background-color: black;
+  transition: 0.3s;
+  width: 100%;
+  height: 30px;
+  align-self: center;
+  &:hover {
+    height: 60px;
+    .menu__item:not(.firstElement) {
+      animation: menuFade 0.5s linear forwards;
+    }
+    .firstElement {
+      transform: translateX(-200px);
+      display: none;
+    }
+  }
+}
+.firstElement {
+  transition: 0.5 linear;
+}
+@keyframes menuFade {
+  0% {
+    opacity: 0;
+  }
+  50% {
+    opacity: 0.5;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+</style>
+
+<script>
+export default {
+  props: ["Menu"]
+};
+</script>
---
+++ pages/enfantMiracle/enfantMiracle.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="enfantMiracle__wrapper">
+    <h1>{{arrayName}}</h1>
+    <figure>
+      <img :src="arrayImage" alt>
+      <figcaption>{{arrayContent}}</figcaption>
+    </figure>
+    <Menu></Menu>
+  </div>
+</template>
+
+
+<style scoped lang="scss">
+.enfantMiracle__wrapper {
+  width: 100vw;
+  height: 100vh;
+  background-color: white;
+  overflow: hidden;
+  background-image: url("../../static/images/enfantBG.png");
+  background-size: cover;
+  background-repeat: no-repeat;
+}
+figure {
+  display: flex;
+  height: 80%;
+  figcaption {
+    background-color: white;
+  }
+}
+h1 {
+  text-align: center;
+  margin-top: 50px;
+  font-size: 20px;
+  transition: ease;
+}
+</style>
+<script>
+import Menu from "~/components/Menu.vue";
+import axios from "axios";
+export default {
+  data() {
+    return {
+      arrayName: null,
+      arrayContent: null,
+      arrayImage: null
+    };
+  },
+  components: {
+    Menu
+  },
+  mounted() {
+    axios.get("http://localhost:7892/api/articles").then(response => {
+      this.arrayName = response.data[2].name;
+      this.arrayContent = response.data[2].content;
+      this.arrayImage = response.data[2].img;
+    });
+  }
+};
+</script>
