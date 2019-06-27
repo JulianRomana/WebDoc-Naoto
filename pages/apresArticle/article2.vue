6df708551c32be9f4eed6ab3494bed8d7651e705
simtaxx <souici.haris@gmail.com>
2019-06-27 13:25:39 +0200

    article anim

--- pages/apresArticle/article2.vue
+++ pages/apresArticle/article2.vue
@@ -3,18 +3,7 @@
     <section id="apresScreen" class="apresScreen">
       <h1>L’APRÈS</h1>
       <img class="firstMove" :src="img">
-      <img class="secondImg" :src="img">
-      <img class="thirdImg" :src="img">
-      <img class="fourthImg" :src="img">
-      <img class="fifthImg" :src="img">
-      <img class="sixthImg" :src="img">
       <article class="txt">
-        <p>{{ txt }}</p>
-      </article>
-      <article class="txt2">
-        <p>{{ txt }}</p>
-      </article>
-      <article class="txt3">
         <p>{{ txt }}</p>
       </article>
       <nuxt-link to="fukushimaVideo">
@@ -76,43 +65,8 @@
     width: 403px;
     height: 402px;
     position: fixed;
-    top: 250px;
-    right: 350px;
-  }
-  .secondImg {
-    display: none;
-    position: fixed;
-    top: 100px;
-    left: 150px;
-    z-index: 3;
-  }
-  .thirdImg {
-    display: none;
-    position: fixed;
-    top: 270px;
-    left: 500px;
-    z-index: 2;
-  }
-  .fourthImg {
-    display: none;
-    position: fixed;
-    top: 130px;
-    left: 1000px;
-    z-index: 1;
-  }
-  .fifthImg {
-    display: none;
-    position: fixed;
-    top: 130px;
-    left: 1000px;
-    z-index: 1;
-  }
-  .sixthImg {
-    display: none;
-    position: fixed;
-    top: 130px;
-    left: 1000px;
-    z-index: 1;
+    top: 150px;
+    right: 750px;
   }
   .txt {
     z-index: 1;
@@ -120,28 +74,8 @@
     width: 585px;
     height: 321px;
     position: fixed;
-    top: 180px;
-    right: 700px;
-  }
-  .txt2 {
-    display: none;
-    z-index: 3;
-    background-color: white;
-    width: 585px;
-    height: 321px;
-    position: fixed;
-    top: 220px;
-    right: 430px;
-  }
-  .txt3 {
-    display: none;
-    z-index: 4;
-    background-color: white;
-    width: 400px;
-    height: 381px;
-    position: fixed;
-    top: 270px;
-    right: 120px;
+    top: 100px;
+    right: 240px;
   }
 }
 </style>
@@ -178,33 +112,11 @@ export default {
     let first = document.querySelector(".firstMove");
     let txt = document.querySelector(".txt");
     let button = document.querySelector(".returnButton");
-    let img2 = document.querySelector(".secondImg");
-    let img3 = document.querySelector(".thirdImg");
-    let img4 = document.querySelector(".fourthImg");
-    let img5 = document.querySelector(".fifthImg");
-    let img6 = document.querySelector(".sixthImg");
-    let txt2 = document.querySelector(".txt2");
-    let txt3 = document.querySelector(".txt3");
     $(window).scroll(function(event) {
       let scroll = $(window).scrollTop();
       console.log(scroll);
       first.style.transform = `perspective(500px) translate3d(0, 0, ${scroll}px)`;
       txt.style.transform = `perspective(500px) translate3d(0, 0, ${scroll}px)`;
-      if (scroll > 502) {
-        scroll = scroll - 502;
-        img2.style.display = "block";
-        txt2.style.display = "block";
-        txt3.style.display = "block";
-        button.style.display = "flex";
-        img2.style.transform = `perspective(500px) translate3d(0, 0, ${scroll}px)`;
-        txt2.style.transform = `perspective(500px) translate3d(0, 0, ${scroll}px)`;
-        txt3.style.transform = `perspective(500px) translate3d(0, 0, ${scroll}px)`;
-      } else {
-        img2.style.display = "none";
-        txt2.style.display = "none";
-        txt3.style.display = "none";
-        button.style.display = "none";
-      }
     });
   }
 };
