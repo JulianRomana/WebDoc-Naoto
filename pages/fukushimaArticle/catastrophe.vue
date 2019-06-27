67787ab759e72e7da0df76068d6de671ed14b85d
simtaxx <souici.haris@gmail.com>
2019-06-27 16:13:11 +0200

    update somes animations

--- pages/fukushimaArticle/catastrophe.vue
+++ pages/fukushimaArticle/catastrophe.vue
@@ -38,7 +38,7 @@
 }
 .fukushima__wrapper {
   width: 100vw;
-  height: 265vh;
+  height: 180vh;
   background-color: white;
   background-image: url("../../static/images/catastrophe/blurryFukushima.png");
   background-size: cover;
@@ -193,39 +193,22 @@ export default {
     let fourth = document.querySelector(".fourthImg");
     $(window).scroll(function(event) {
       let scroll = $(window).scrollTop();
-      let scrollDiv = scroll / 100;
-      let scrollSecond = scrollDiv - 3;
-      console.log(scrollDiv);
-      first.style.transform = `scale(${1 + scrollDiv})`;
-      txt.style.transform = `scale(${1 + scrollDiv})`;
-      let size = 3.2;
-      if (scrollDiv > size) {
-        first.style.display = "none";
-        txt.style.display = "none";
-        aside.style.display = "none";
+      first.style.transform = `perspective(500px) translate3d(0, 0, ${scroll}px)`;
+      txt.style.transform = `perspective(500px) translate3d(0, 0, ${scroll}px)`;
+      if (scroll > 502) {
+        scroll = scroll - 502;
+        console.log(scroll);
         second.style.display = "block";
-        second.style.transform = `scale(${scrollSecond})`;
-        if (scrollSecond > size) {
-          second.style.display = "none";
-          third.style.display = "block";
-          scrollSecond = scrollSecond - 2.6;
-          third.style.transform = `scale(${scrollSecond})`;
-          if (scrollSecond > size) {
-            third.style.display = "none";
-            fourth.style.display = "block";
-            scrollSecond = scrollSecond - 2.6;
-            fourth.style.transform = `scale(${scrollSecond})`;
-            if (scrollSecond > size) {
-              fourth.style.display = "none";
-            }
-          }
-        }
+        third.style.display = "block";
+        fourth.style.display = "block";
+        aside.style.display = "none";
+        second.style.transform = `perspective(500px) translate3d(0, 0, ${scroll}px)`;
+        third.style.transform = `perspective(500px) translate3d(0, 0, ${scroll}px)`;
+        fourth.style.transform = `perspective(500px) translate3d(0, 0, ${scroll}px)`;
       } else {
-        first.style.display = "block";
-        txt.style.display = "block";
-        aside.style.display = "block";
         second.style.display = "none";
         third.style.display = "none";
+        aside.style.display = "block";
         fourth.style.display = "none";
       }
     });
